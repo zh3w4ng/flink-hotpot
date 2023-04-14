f0364711c9aa959e4a95aa204fd6fbc8201e60fc
Zhe Jeu <zhe.wang@highspot.com>
2023-04-14 11:17:29 -0700

    add joda-time to parse the datetime string

--- src/main/java/com/highspot/prismo/hotpot/job/mapfunc/StringToM2kEvent.java
+++ src/main/java/com/highspot/prismo/hotpot/job/mapfunc/StringToM2kEvent.java
@@ -4,6 +4,7 @@ import org.apache.flink.api.common.functions.MapFunction;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.joda.time.DateTime;
 import com.highspot.prismo.hotpot.schema.M2kEvent;
 
 public class StringToM2kEvent implements MapFunction<String, M2kEvent> {
@@ -17,6 +18,8 @@ public class StringToM2kEvent implements MapFunction<String, M2kEvent> {
         JsonNode node = new ObjectMapper().readTree(jsonString);
         M2kEvent event = new ObjectMapper().treeToValue(node, M2kEvent.class);
         System.out.println(event.getId());
+        DateTime dt = new DateTime(event.getCreatedAt().get$date());
+        System.out.println(dt.getDayOfMonth());
         System.out.println("#########");
         return event;
     }
